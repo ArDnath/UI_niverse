9ae0aaad0db42a2b1fd03be8ffcb889145d9bcce
ariyaman <ariyamandebnath.ad@gmail.com>
2025-11-10 01:07:18 +0530

    chore: minor UX changes

--- UIniverse/src/App.tsx
+++ UIniverse/src/App.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "./components/ui/Navbar";
-import Footer from "./components/ui/Footer";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
